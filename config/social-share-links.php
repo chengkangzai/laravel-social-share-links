424c6dc2034819ad59411d8b4893bf08fef25a47
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2023-01-28 21:09:50 +0800

    remove useless file

--- config/social-share-links.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// config for Chengkangzai/LaravelSocialShareLinks
-return [
-
-];
--- src/LaravelSocialShareLinksServiceProvider.php
+++ src/LaravelSocialShareLinksServiceProvider.php
@@ -9,13 +9,6 @@ class LaravelSocialShareLinksServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package
-            ->name('laravel-social-share-links')
-            ->hasConfigFile();
+        $package->name('laravel-social-share-links');
     }
 }
